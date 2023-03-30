fef903202a86f39da2063930e54b20e5da53ded6
ArmandoCivini <tcivini@gmail.com>
2023-03-30 09:20:41 -0300

    added multiprocessing to agency communication

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
-## Parte 2: Repaso de Comunicaciones
+## Parte 3: Repaso de Concurrencia
 
-Las secciones de repaso del trabajo práctico plantean un caso de uso denominado **Lotería Nacional**. Para la resolución de las mismas deberá utilizarse como base al código fuente provisto en la primera parte, con las modificaciones agregadas en el ejercicio 4.
+### Ejercicio N°8:
+Modificar el servidor para que permita aceptar conexiones y procesar mensajes en paralelo.
+En este ejercicio es importante considerar los mecanismos de sincronización a utilizar para el correcto funcionamiento de la persistencia.
 
-### Ejercicio N°7:
-Modificar los clientes para que notifiquen al servidor al finalizar con el envío de todas las apuestas y así proceder con el sorteo.
-Inmediatamente después de la notificacion, los clientes consultarán la lista de ganadores del sorteo correspondientes a su agencia.
-Una vez el cliente obtenga los resultados, deberá imprimir por log: `action: consulta_ganadores | result: success | cant_ganadores: ${CANT}`.
-
-El servidor deberá esperar la notificación de las 5 agencias para considerar que se realizó el sorteo e imprimir por log: `action: sorteo | result: success`.
-Luego de este evento, podrá verificar cada apuesta con las funciones `load_bets(...)` y `has_won(...)` y retornar los DNI de los ganadores de la agencia en cuestión. Antes del sorteo, no podrá responder consultas por la lista de ganadores.
-Las funciones `load_bets(...)` y `has_won(...)` son provistas por la cátedra y no podrán ser modificadas por el alumno.
+En caso de que el alumno implemente el servidor Python utilizando _multithreading_,  deberán tenerse en cuenta las [limitaciones propias del lenguaje](https://wiki.python.org/moin/GlobalInterpreterLock).
 
--- server/common/server.py
+++ server/common/server.py
@@ -3,7 +3,7 @@ import logging
 import signal
 from common.protocol import read_batch, long_write, get_ip
 from common.utils import store_bets, load_bets, has_won
-from time import sleep
+import multiprocessing as mp
 
 BET_TYPE = b'\x01'
 FINISHED_TYPE = b'\x02'
@@ -12,6 +12,72 @@ WINNER_TYPE = b'\x05'
 FINISHED_WINNERS_TYPE = b'\x06'
 NUMBER_OF_AGENCIES = 2
 
+class BatchOrEnd:
+    def __init__(self, batch, end=False, socket=None):
+        self.batch = batch
+        self.end = end
+        self.socket = socket
+
+def bet_loader(bet_q):
+    finished_agencies = 0
+    waiting_clients = {}
+
+    while finished_agencies < NUMBER_OF_AGENCIES:
+        batch = bet_q.get()
+        if batch.end:
+            waiting_clients[get_ip(batch.socket)] = batch.socket
+            finished_agencies += 1
+            continue
+        store_bets(batch.batch)
+
+    bets = load_bets()
+    for bet in bets:
+        if has_won(bet):
+            logging.info(f'action: WINNER | result: success | dni: ${bet.document} | numero: ${bet.number}')
+            send_won_message(bet, waiting_clients[bet.agency])
+    
+    send_finished_winners_message(waiting_clients)
+
+def send_won_message(bet, skt):
+    message = bytearray()
+    message += WINNER_TYPE
+    message += int(bet.document).to_bytes(4, byteorder='big', signed=True)
+    long_write(skt, message)
+
+
+def send_finished_winners_message(waiting_clients):
+    for agency in waiting_clients:
+        skt = waiting_clients[agency]
+        long_write(skt, FINISHED_WINNERS_TYPE)
+        skt.close()
+
+def handle_client_connection(client_sock, bet_q):
+    """
+    Read message from a specific client socket and closes the socket
+
+    If a problem arises in the communication with the client, the
+    client socket will also be closed
+    """
+    finished_betting = False
+    while not finished_betting:
+        try:
+            msg = client_sock.recv(1)
+            if msg == BET_TYPE:
+                batch = read_batch(client_sock)
+                bet_q.put(BatchOrEnd(batch))
+                # store_bets(batch)
+                logging.info(f'action: batch_almacenad0 | result: success')
+            elif msg == FINISHED_TYPE:
+                finished_betting = True
+                logging.info(f'action: ended_bets | result: success | id: {get_ip(client_sock)}')
+                bet_q.put(BatchOrEnd(None, end=True, socket=client_sock))
+                return
+        except OSError as e:
+            logging.error("action: receive_message | result: fail | error: {e}")
+        finally:
+            if not finished_betting:
+                long_write(client_sock, CONFIRM_TYPE)
+
 class Server:
     def __init__(self, port, listen_backlog):
         # Initialize server socket
@@ -19,8 +85,9 @@ class Server:
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         self.running = True
-        self.waiting_clients = {}
         signal.signal(signal.SIGTERM, self.graceful_shutdown)
+
+    
 
     def run(self):
         """
@@ -32,63 +99,25 @@ class Server:
         """
 
         # the server
-        while self.running and len(self.waiting_clients) < NUMBER_OF_AGENCIES:
+        bet_q = mp.Manager().Queue()
+        p = mp.Process(target=bet_loader, args=(bet_q,))
+        p.start()
+        contacted_agencies = 0
+        processes = []
+        while self.running and contacted_agencies < NUMBER_OF_AGENCIES:
             client_sock = self.__accept_new_connection()
-            if client_sock: self.__handle_client_connection(client_sock)
+            if client_sock:  
+                new_p = mp.Process(target=handle_client_connection, args=(client_sock,bet_q,))
+                processes.append(new_p)
+                new_p.start()
+                contacted_agencies += 1
         
-        if not self.running:
-            return
-        
-        
-        bets = load_bets()
-        for bet in bets:
-            if has_won(bet):
-                logging.info(f'action: WINNER | result: success | dni: ${bet.document} | numero: ${bet.number}')
-                self.send_won_message(bet)
-        
-        self.send_finished_winners_message()
+        for new_p in processes:
+            new_p.join()
+        p.join()
+        # logging.info(f'FIN')
+        return
 
-
-
-    def send_won_message(self, bet):
-        skt = self.waiting_clients[bet.agency]
-        message = bytearray()
-        message += WINNER_TYPE
-        message += int(bet.document).to_bytes(4, byteorder='big', signed=True)
-        long_write(skt, message)
-
-
-    def send_finished_winners_message(self):
-        for agency in self.waiting_clients:
-            skt = self.waiting_clients[agency]
-            long_write(skt, FINISHED_WINNERS_TYPE)
-
-
-    def __handle_client_connection(self, client_sock):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
-        finished_betting = False
-        while not finished_betting:
-            try:
-                msg = client_sock.recv(1)
-                if msg == BET_TYPE:
-                    batch = read_batch(client_sock)
-                    store_bets(batch)
-                    logging.info(f'action: batch_almacenad0 | result: success')
-                elif msg == FINISHED_TYPE:
-                    finished_betting = True
-                    logging.info(f'action: ended_bets | result: success | id: {get_ip(client_sock)}')
-            except OSError as e:
-                logging.error("action: receive_message | result: fail | error: {e}")
-            finally:
-                if not finished_betting:
-                    long_write(client_sock, CONFIRM_TYPE)
-
-        self.waiting_clients[get_ip(client_sock)] = client_sock
 
     def __accept_new_connection(self):
         """
@@ -115,12 +144,8 @@ class Server:
         """
         self.running = False
         logging.info(f'action: closing_sockets | result: in_progress')
-        for client in self.waiting_clients:
-            client.close()
         self._server_socket.close()
         logging.info(f'action: closing_sockets | result: success')
 
     def __del__(self):
-        for client in self.waiting_clients:
-            self.waiting_clients[client].close()
         self._server_socket.close()
